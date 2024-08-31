6700c1011a4f08333cc4a7524e1fb27664f06807
Shae Jacobs <shae.jacobs01@gmail.com>
2024-08-31 22:49:49 +1200

    created models, database tables and starting on seeds

---
+++ server/db/migrations/20240831103946_posts.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  return knex.schema.createTable('posts', (table) => {
+    table.increments('id').primary()
+    table.integer('user_id')
+    table.integer('likes')
+    table.string('post_image')
+    table.string('comments')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema.dropTable('posts')
+}
